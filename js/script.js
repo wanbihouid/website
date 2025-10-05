2e054d366de349ccf5c653d574e019c6952d5fe1
万币侯 <80379075+wanbihouid@users.noreply.github.com>
2025-10-05 16:59:12 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
-    const $img = $('#main_img');
+    const $main_img = $('#main_img');
     const img_1 = '/img/main.jpg';
     const img_2 = '/img/icon.jpg';
     let now_img = 1;
 
-    $img.on('click', function() {
-        $img.fadeOut(500, function() {
-            $img.attr('src', now_img ? img_2 : img_1);
+    $main_img.on('click', function() {
+        $main_img.fadeOut(500, function() {
+            $main_img.attr('src', now_img ? img_2 : img_1);
             now_img = 0;
-            $img.fadeIn(500);
+            $main_img.fadeIn(500);
         });
     });
 });
