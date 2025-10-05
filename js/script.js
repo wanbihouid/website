b50b1e79469472655c50ea7603be3d299ed3e2a5
万币侯 <80379075+wanbihouid@users.noreply.github.com>
2025-10-05 17:29:47 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,9 +7,9 @@ $(document).ready(function() {
     $main_img.on('click', function() {
         $main_img.hide(0, function() {
             if(now_img == true){
-                $main_img.css('opacity',0.1);
+                $main_img.css({'opacity':0.1,'padding':30%,'border':'1px dashed #ffffff'});
             }else{
-                $main_img.css('opacity',1);
+                $main_img.css({'opacity':1,'padding':'unset','border':'none'});
             }
             $main_img.attr('src', now_img ? icon_jpg : main_jpg);
             now_img = !now_img; //切换0和1
